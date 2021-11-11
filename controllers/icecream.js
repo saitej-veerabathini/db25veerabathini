fee7c28b8e3dde10926bf60af43c0881536f3110
saitej-veerabathini <saitej.veerabathini@gmail.com>
2021-11-10 21:34:20 -0600

    changed icecream.js for post

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -11,6 +11,19 @@ exports.icecream_list = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }
 }; 
+
+// VIEWS
+// Handle a show all view
+exports.icecream_view_all_Page = async function (req, res) {
+    try {
+        theicecream = await icecream.find();
+        res.render('icecream', { title: 'icecream Search Results', results: theicecream });
+    }
+    catch (err) {
+        res.send(`{"error": ${err}}`)
+        res.status(500);
+    }
+};
  
 // for a specific icecream. 
 exports.icecream_detail = function(req, res) { 
@@ -18,9 +31,25 @@ exports.icecream_detail = function(req, res) {
 }; 
  
 // Handle icecream create on POST. 
-exports.icecream_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream create POST'); 
-}; 
+exports.icecream_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.brand = req.body.brand; 
+    document.flavour = req.body.flavour; 
+    document.cost = req.body.cost;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
  
 // Handle icecream delete form on DELETE. 
 exports.icecream_delete = function(req, res) { 
--- routes/icecream.js
+++ routes/icecream.js
@@ -1,5 +1,10 @@
 var express = require('express');
+const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
+
+/* GET icecream */ 
+router.get('/', icecream_controlers.icecream_view_all_Page );
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
